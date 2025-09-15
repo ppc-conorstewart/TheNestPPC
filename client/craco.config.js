db9ebc283b1fbd6149048db6b3815dc92a259f7e
ppc-conorstewart <itzconstar@gmail.com>
2025-09-14 20:15:16 -0600

    Update craco.config.js

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,34 +1,24 @@
-// client/craco.config.js
-const { ESLintWebpackPlugin } = require('eslint-webpack-plugin');
-
+// ==============================
+// FILE: client/craco.config.js
+// ==============================
 module.exports = {
-  // 1) remove CRA's ESLint plugin from webpack
   webpack: {
-       configure: (webpackConfig) => {
-     webpackConfig.plugins = webpackConfig.plugins.filter(plugin => {
-       // drop any plugin whose constructor is named "ESLintWebpackPlugin"
-       return !(
-         plugin &&
-         plugin.constructor &&
-         plugin.constructor.name === 'ESLintWebpackPlugin'
-       );
-     });
-     return webpackConfig;
-   },
+    configure: (webpackConfig) => {
+      webpackConfig.plugins = webpackConfig.plugins.filter((plugin) => {
+        return !(
+          plugin &&
+          plugin.constructor &&
+          plugin.constructor.name === 'ESLintWebpackPlugin'
+        );
+      });
+      return webpackConfig;
+    },
   },
-
-  // 2) your existing devServer proxy bits
   devServer: {
     allowedHosts: 'all',
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-      '/auth': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
+      '/api': { target: 'http://localhost:3001', changeOrigin: true },
+      '/auth': { target: 'http://localhost:3001', changeOrigin: true },
     },
   },
 };
